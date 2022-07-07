814925852bfe828b20fdf1e22581edd5676b6042
Tanjim Hossain <tangimhossain1@gmail.com>
2022-07-08 00:31:48 +0600

    97 done - day 1

--- leetcode/Rust/daily/problems/97/src/lib.rs
+++ leetcode/Rust/daily/problems/97/src/lib.rs
@@ -1,32 +1,32 @@
+use std::collections::HashMap;
+
 pub struct Solution;
 
 impl Solution {
     pub fn is_interleave(s1: String, s2: String, s3: String) -> bool {
-        if s1.len() + s2.len() != s3.len() {
+        let (l1, l2, l3) = (s1.len(), s2.len(), s3.len());
+        if l1 + l2 != l3 {
             return false;
         }
 
-        let mut s1 = s1.chars().peekable();
-        let mut s2 = s2.chars().peekable();
-        let mut s3 = s3.chars().peekable();
+        let [s1, s2, s3]: [Vec<char>; 3] = [s1, s2, s3].map(|string| string.chars().collect());
 
-        while let Some(c3) = s3.peek() {
-            if s1.next_if_eq(&c3).is_some() {
-                s3.next();
-                continue;
+        let mut cache: HashMap<(usize, usize), bool> = HashMap::new();
+
+        cache.insert((l1, l2), true);
+
+        for i1 in (0..=l1).rev() {
+            for i2 in (0..=l2).rev() {
+                if i1 < l1 && s1[i1] == s3[i1 + i2] && cache.get(&(i1 + 1, i2)).is_some() {
+                    cache.insert((i1, i2), true);
+                }
+                if i2 < l2 && s2[i2] == s3[i1 + i2] && cache.get(&(i1, i2 + 1)).is_some() {
+                    cache.insert((i1, i2), true);
+                }
             }
-            if s2.next_if_eq(&c3).is_some() {
-                s3.next();
-                let tmp = s1;
-                s1 = s2;
-                s2 = tmp;
-                // (s1, s2) = (s2, s1);
-                continue;
-            }
-            return false;
         }
 
-        true
+        return cache.get(&(0, 0)).is_some();
     }
 }
 
@@ -36,37 +36,70 @@ mod tests {
 
     #[test]
     fn testcase_1() {
-        let s1 = "aabcc".to_string();
-        let s2 = "dbbca".to_string();
-        let s3 = "aadbbcbcac".to_string();
+        let s1 = "aabcc".to_owned();
+        let s2 = "dbbca".to_owned();
+        let s3 = "aadbbcbcac".to_owned();
         let result = Solution::is_interleave(s1, s2, s3);
         assert_eq!(result, true);
     }
 
     #[test]
     fn testcase_2() {
-        let s1 = "aabcc".to_string();
-        let s2 = "dbbcc".to_string();
-        let s3 = "aadbbcbcac".to_string();
+        let s1 = "aabcc".to_owned();
+        let s2 = "dbbcc".to_owned();
+        let s3 = "aadbbcbcac".to_owned();
         let result = Solution::is_interleave(s1, s2, s3);
         assert_eq!(result, false);
     }
 
     #[test]
     fn testcase_3() {
-        let s1 = "aabcc".to_string();
-        let s2 = "dbbca".to_string();
-        let s3 = "aadbbbaccc".to_string();
+        let s1 = "aabcc".to_owned();
+        let s2 = "dbbca".to_owned();
+        let s3 = "aadbbbaccc".to_owned();
         let result = Solution::is_interleave(s1, s2, s3);
         assert_eq!(result, false);
     }
 
     #[test]
     fn testcase_4() {
-        let s1 = "".to_string();
-        let s2 = "".to_string();
-        let s3 = "".to_string();
+        let s1 = "".to_owned();
+        let s2 = "".to_owned();
+        let s3 = "".to_owned();
         let result = Solution::is_interleave(s1, s2, s3);
         assert_eq!(result, true);
+    }
+
+    #[test]
+    fn testcase_5() {
+        let s1 = "aa".to_owned();
+        let s2 = "ab".to_owned();
+        let s3 = "aaba".to_owned();
+        let result = Solution::is_interleave(s1, s2, s3);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn testcase_6() {
+        assert_eq!(
+            Solution::is_interleave(
+                "aabcc".to_owned(),
+                "dbbca".to_owned(),
+                "aadbbcbcac".to_owned()
+            ),
+            true
+        );
+    }
+
+    #[test]
+    fn testcase_7() {
+        assert_eq!(
+            Solution::is_interleave(
+                "aabcc".to_owned(),
+                "dbbca".to_owned(),
+                "aadbbbaccc".to_owned()
+            ),
+            false
+        );
     }
 }
